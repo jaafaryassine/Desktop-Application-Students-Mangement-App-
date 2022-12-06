b28f6cabc4e7937e4c18bab197fc6c671068a3fb
oumayma-yakoubi <113624636+oumayma-yakoubi@users.noreply.github.com>
2022-12-06 23:39:44 +0100

    send Email with attachment

---
+++ ProjectGl/src/main/java/com/sendemail/SendMail.java
@@ -0,0 +1,22 @@
+package com.sendemail;
+
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeBodyPart;
+
+
+
+public class SendMail {
+
+}
--- ProjectGl/src/main/java/org/example/Main.java
+++ ProjectGl/src/main/java/org/example/Main.java
@@ -1,7 +1,101 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 public class Main {
     public static void main(String[] args) {
         LoginPage app = new LoginPage();
+        
+        // Recipient's email ID needs to be mentioned.
+        String to = "ouma.yakoubi20@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "ya.oumayma@gmail.com";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass 
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("ya.oumayma@gmail.com", "dgwygrsuckypwftk");
+
+            }
+
+        });
+        //session.setDebug(true);
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            Multipart multipart = new MimeMultipart();
+
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+
+            MimeBodyPart textPart = new MimeBodyPart();
+
+            try {
+
+                File f =new File("C:\\Users\\Hp\\Downloads\\conception UML.pdf");
+
+                attachmentPart.attachFile(f);
+                textPart.setText("This is text");
+                multipart.addBodyPart(textPart);
+                multipart.addBodyPart(attachmentPart);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            message.setContent(multipart);
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+    
+        
+        
+        
+        
     }
 }
